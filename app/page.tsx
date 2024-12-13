5591f58567ef2974601b72175a09354f2b4d143f
Zhila <ministrov2018@gmail.com>
2024-12-13 13:55:40 +0300

    feature - Adjust eslint to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+const f: number = 4;
+
+console.log(f);
+
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -18,6 +22,8 @@ export default function Home() {
             Get started by editing <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly-2</li>
+          <li>Save and see your changes instantly-3</li>
         </ol>
 
         <div className={styles.ctas}>
