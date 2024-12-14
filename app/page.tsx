a58a9d6f34cb813321d8dfddc9bf0169b0b869ef
Zhila <ministrov2018@gmail.com>
2024-12-14 13:27:11 +0300

    feature - Add root colors variables to all the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <div className={styles.page}></div>
+    <div>
+      dfkdjfskdfjsdkfjsd
+    </div>
   );
 }
