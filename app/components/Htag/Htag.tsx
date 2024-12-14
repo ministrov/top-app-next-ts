5dfb04be246becd015fa08503d904858d4af5d31
Zhila <ministrov2018@gmail.com>
2024-12-14 17:57:09 +0300

    feature - End up creating Htag component

--- app/components/Htag/Htag.tsx
+++ app/components/Htag/Htag.tsx
@@ -7,24 +7,13 @@ import styles from './Htag.module.css';
 // console.log(styles.__checksum);
 
 export default function Htag({ tag, children }: HtagProps): JSX.Element {
-
-    // First way of render multiple htag
-    // return (
-    //     <>
-    //         {tag === 'h1' && <h1 className={styles.htag}>{children}</h1>}
-    //         {tag === 'h2' && <h2 className={styles.htag__h2}>{children}</h2>}
-    //         {tag === 'h3' && <h3 className={styles.htag__h2}>{children}</h3>}
-    //     </>
-    // )
-
-    // Second way
     switch (tag) {
         case 'h1':
-            return <h1 className={styles.htag}>{children}</h1>;
+            return <h1 className={styles.h1}>{children}</h1>;
         case 'h2':
-            return <h2 className={styles.htag__h2}>{children}</h2>;
+            return <h2 className={styles.h2}>{children}</h2>;
         case 'h3':
-            return <h3 className={styles.htag__h3}>{children}</h3>;
+            return <h3 className={styles.h3}>{children}</h3>;
         default:
             return <></>;
     }
