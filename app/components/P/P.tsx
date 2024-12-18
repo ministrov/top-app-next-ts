9408609e604abd883668d84500197221ed504a95
Zhila <ministrov2018@gmail.com>
2024-12-18 11:07:24 +0300

    feature - Paragraph component commit

--- app/components/P/P.tsx
+++ app/components/P/P.tsx
@@ -3,13 +3,13 @@ import { ParagraphProps } from './P.props';
 import cn from 'classnames';
 import styles from './P.module.css';
 
-const Paragraph = ({ size = 'medium', children }: ParagraphProps) => {
+const Paragraph = ({ size = 'medium', children, ...props }: ParagraphProps) => {
     return (
         <p className={cn(styles.p, {
             [styles.small]: size === 'small',
             [styles.medium]: size === 'medium',
             [styles.large]: size === 'large',
-        })}>
+        })} {...props}>
             {children}
         </p>
     )
