ea7931e10364c67f8b3bc9193e989cf8dd466169
Zhila <ministrov2018@gmail.com>
2024-12-19 17:06:40 +0300

    feature - Use state hook to home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 'use client';
 
+import { useState } from 'react';
 import { Button } from './components/Button/Button';
 import Htag from './components/Htag/Htag';
 import P from './components/P/P';
 import Tag from './components/Tag/Tag';
 
 export default function Home() {
+  const [counter, setCounter] = useState<number>(0);
   const onclick = () => {
     console.log('clicked');
+    setCounter(x => x + 1);
   }
   return (
     <div>
       dfkdjfskdfjsdkfjsd
 
       <Htag tag='h3'>
-        Some text
+        {counter}
       </Htag>
 
       <Button appearence='primary' onClick={onclick} className='sfsfsdf' arrow='right'>Узнать подробнее</Button>
