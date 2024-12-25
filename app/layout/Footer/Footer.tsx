3324095ec1ac4c6fe5263df4f83d956368f0d5d0
Zhila <ministrov2018@gmail.com>
2024-12-25 17:03:11 +0300

    feature - Finish creating layout component

--- app/layout/Footer/Footer.tsx
+++ app/layout/Footer/Footer.tsx
@@ -1,8 +1,15 @@
-// import styles from './Footer.module.css';
+import Link from 'next/link';
+import { FooterProps } from './Footer.props';
+import cn from 'classnames';
+import styles from './Footer.module.css';
 
-const Footer = ({ ...props }) => {
+const Footer = ({ className, ...props }: FooterProps) => {
     return (
-        <footer {...props}>Footer</footer>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <span>OwlTop © 2020 - {new Date().getFullYear()} Все права защищены</span>
+            <Link href={'#'}>Пользовательское соглашение</Link>
+            <Link href={'#'}>Политика конфиденциальности</Link>
+        </footer>
     )
 }
 
