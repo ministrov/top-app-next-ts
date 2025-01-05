310d8985ddd88853c0754ab38b5f2ec9b51e8842
Zhila <ministrov2018@gmail.com>
2025-01-05 16:35:54 +0300

    refactore - Move getMenu function to api folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-import { API } from '@/api/api';
-import { MenuItem } from '@/interfaces/menu.interface';
-
-async function getMenu(firstCategory: number): Promise<MenuItem[]> {
-  const response = await fetch(API.topPage.find, {
-    method: 'POST',
-    body: JSON.stringify({
-      firstCategory
-    }),
-    headers: new Headers({ 'content-type': 'application/json' })
-  });
-
-  return response.json();
-}
+import { getMenu } from '@/api/menu';
 
 export default async function Home() {
   const menu = await getMenu(0);
+  console.log(menu);
+  console.log(typeof menu);
   console.log('I am Home!!!');
 
   return (
