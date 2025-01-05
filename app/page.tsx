8325fb3d75769f9a9e1056e8eea51bb6f2f3f1cf
Zhila <ministrov2018@gmail.com>
2025-01-05 16:27:35 +0300

    feature - Send a POST request to api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-export default function Home() {
+import { API } from '@/api/api';
+import { MenuItem } from '@/interfaces/menu.interface';
+
+async function getMenu(firstCategory: number): Promise<MenuItem[]> {
+  const response = await fetch(API.topPage.find, {
+    method: 'POST',
+    body: JSON.stringify({
+      firstCategory
+    }),
+    headers: new Headers({ 'content-type': 'application/json' })
+  });
+
+  return response.json();
+}
+
+export default async function Home() {
+  const menu = await getMenu(0);
   console.log('I am Home!!!');
 
   return (
     <div>
       {'Home Page'}
+      <ul>
+        {JSON.stringify(menu)}
+        <li>{menu.length}</li>
+      </ul>
     </div>
   );
 }
