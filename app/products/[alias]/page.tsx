a642d9f80a38bd82d4386c79f0393f173e079fac
Zhila <ministrov2018@gmail.com>
2025-01-06 12:57:15 +0300

    refactore - Move menu component to layout folder

--- app/products/[alias]/page.tsx
+++ app/products/[alias]/page.tsx
@@ -1,18 +1,18 @@
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { getPage } from '@/api/page';
-// import { getMenu } from '@/api/menu';
+import { getMenu } from '@/api/menu';
 
 export const metadata: Metadata = {
     title: 'Страница'
 }
 
-// export const generateStaticParams = async () => {
-//     const menu = await getMenu(0);
-//     return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })));
-// }
+export const generateStaticParams = async () => {
+    const menu = await getMenu(0);
+    return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })));
+}
 
-const ProductsPage = async ({ params }: { params: { alias: string } }) => {
+export const ProductsPage = async ({ params }: { params: { alias: string } }) => {
     console.log(params);
     const page = await getPage(params.alias);
     if (!page) {
@@ -22,5 +22,3 @@ const ProductsPage = async ({ params }: { params: { alias: string } }) => {
         <div>{page.title}</div>
     )
 }
-
-export default ProductsPage;
