57072a3faf41ad19537bfe2752e7e9c7a6f87ba4
Zhila <ministrov2018@gmail.com>
2025-01-07 13:33:39 +0300

    feature - Create a list from mocks array of people to render item

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next';
+import { People } from '@/interfaces/peoples.interface';
+import { peoples } from '@/mocks';
 import styles from './aboutPage.module.css';
 
 export const metadata: Metadata = {
@@ -6,14 +8,24 @@ export const metadata: Metadata = {
     description: 'Узнайте больше о нашей команде и миссии'
 }
 
+export const getImageUrl = (person: People): string => (`https://i.imgur.com/${person.imageId}s.jpg`);
+
 const AboutPage = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.header}>Header</div>
-            <div className={styles.body}>Body</div>
-            <div className={styles.sidebar}>SideBar</div>
-            <div className={styles.footer}>Footer</div>
-        </div>
+        <>
+            <div className={styles.container}>
+                <div className={styles.header}>Header</div>
+                <div className={styles.body}>Body</div>
+                <div className={styles.sidebar}>SideBar</div>
+                <div className={styles.footer}>Footer</div>
+            </div>
+
+            <ul>
+                {peoples.map(people => (
+                    <li key={people.id}>{people.name}</li>
+                ))}
+            </ul>
+        </>
     )
 }
 
