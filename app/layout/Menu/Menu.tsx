15d74d4178cc600adff9023f805b8bcb0a1ea27d
Zhila <ministrov2018@gmail.com>
2025-01-07 11:10:43 +0300

    feature - Start create styling for menu component

--- app/layout/Menu/Menu.tsx
+++ app/layout/Menu/Menu.tsx
@@ -2,33 +2,26 @@ import { JSX } from 'react';
 import { getMenu } from '@/api/menu';
 import { FirstLevelMenuItem, PageItem } from '@/interfaces/menu.interface';
 import { TopLevelCategory } from '@/interfaces/page.interface';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import GoodsIcon from './icons/goods.svg';
+import CoursesIcon from './CoursesIcon/CoursesIcon';
+import ServicesIcon from './ServicesIcon/ServicesIcon';
+import BooksIcon from './BooksIcon/BooksIcon';
+import GoodsIcon from './GoodsIcon/GoodsIcon';
+
 import cn from 'classnames';
 import styles from './Menu.module.css';
 
 const firstLevelMenu: FirstLevelMenuItem[] = [
-    { route: 'courses', name: 'Курсы', icon: CoursesIcon, id: TopLevelCategory.Courses },
-    { route: 'services', name: 'Сервисы', icon: ServicesIcon, id: TopLevelCategory.Services },
-    { route: 'books', name: 'Книги', icon: BooksIcon, id: TopLevelCategory.Books },
-    { route: 'goods', name: 'Товары', icon: GoodsIcon, id: TopLevelCategory.Goods },
-];
+    { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+    { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+    { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+    { route: 'goods', name: 'Товары', icon: <GoodsIcon />, id: TopLevelCategory.Goods },
+]
 
 const firstCategory = TopLevelCategory.Courses;
 
 const menu = await getMenu(firstCategory);
 
-console.log(menu);
-
 export const Menu = (): JSX.Element => {
-
-    // console.log(JSON.stringify(menu));
-    // console.log(menu[0]);
-    // console.log(Array.isArray(menu));
-    // console.log('I am Home!!!');
-
     const buildFirstLevel = () => {
         return (
             <>
@@ -69,15 +62,13 @@ export const Menu = (): JSX.Element => {
 
     const buildThirdLevel = (pages: PageItem[], route: string) => {
         return (
-            <div>
-                {pages.map(page => (
-                    <a key={page.id} href={`/${route}/${page.alias}`} className={cn(styles.thirdLevel, {
-                        [styles.thirdLevelActive]: true
-                    })}>
-                        {page.title}
-                    </a>
-                ))}
-            </div>
+            pages.map(page => (
+                <a key={page.id} href={`/${route}/${page.alias}`} className={cn(styles.thirdLevel, {
+                    [styles.thirdLevelActive]: true
+                })}>
+                    {page.title}
+                </a>
+            ))
         );
     }
 
