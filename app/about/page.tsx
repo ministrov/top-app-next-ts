c9223ad4a863a3dfa5bf0c85e9578b1463c4b363
Zhila <ministrov2018@gmail.com>
2025-01-07 12:46:07 +0300

    feature - Create mocks for peoples array of objects

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,17 +14,6 @@ const AboutPage = () => {
             <div className={styles.sidebar}>SideBar</div>
             <div className={styles.footer}>Footer</div>
         </div>
-        // <div className={styles.container}>
-        //     <div>1</div>
-        //     <div>2</div>
-        //     <div>3</div>
-        //     <div>4</div>
-        //     <div>5</div>
-        //     <div>6</div>
-        //     <div>7</div>
-        //     <div>8</div>
-        //     <div>9</div>
-        // </div>
     )
 }
 
