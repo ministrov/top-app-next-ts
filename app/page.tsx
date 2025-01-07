159f4bb7abe2b36c1ce220ec4b7da6f4a3288aa2
Zhila <ministrov2018@gmail.com>
2025-01-07 17:49:37 +0300

    feature - Keep on operating on routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+// import Link from 'next/link';
+
 export default async function Home() {
   return (
     <div>
       {'Home Page'}
+
+      {/* <Link href={'/about'}>to About page</Link> */}
     </div>
   );
 }
