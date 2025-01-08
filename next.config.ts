173eb9c51da42a3492aca5370d3f9f3464fbbaac
Zhila <ministrov2018@gmail.com>
2025-01-08 13:53:09 +0300

    refactore - Change and adjust next configuration file to use svg as a component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  webpack(config, _options) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 }
 
 export default nextConfig;
