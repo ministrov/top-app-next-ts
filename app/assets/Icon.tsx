840b4d9a1c67539b876b1d39f1459fcc84e5ae95
Zhila <ministrov2018@gmail.com>
2025-01-08 11:25:41 +0300

    refactore - Keep on refactoring on icons

--- app/assets/Icon.tsx
+++ app/assets/Icon.tsx
@@ -1,5 +1,9 @@
-export const Icon = () => {
-    return (
-        <div>Icon</div>
-    )
+import ArrowIcon from './icons/arrow.svg';
+import StarIcon from './icons/star.svg';
+
+export const Icon = {
+    ArrowIcon,
+    StarIcon
 }
+
+export type IconType = keyof typeof Icon;
