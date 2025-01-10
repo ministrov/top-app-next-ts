741248f65a7d95808610c4f8d8ad02f392ab5bdb
Zhila <ministrov2018@gmail.com>
2025-01-10 11:55:25 +0300

    fix - Keep on fixing on params type error on page products

--- app/products/[alias]/page.tsx
+++ app/products/[alias]/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+// import { PageProps } from '@/.next/types/app/page';
 import { notFound } from 'next/navigation';
 import { getPage } from '@/api/page';
 import { getMenu } from '@/api/menu';
@@ -13,12 +14,12 @@ export const generateStaticParams = async () => {
 }
 
 export default async function PageProduct({ params }: { params: { alias: string } }) {
-    const { alias } = params;
-    const page = await getPage(alias);
+    // const { alias } = params;
+    const page = await getPage(params.alias);
     if (!page) {
         notFound();
     }
     return (
-        <div>{page.title}</div>
+        <div>{`Страница с alias ${params.alias}`}</div>
     )
 }
