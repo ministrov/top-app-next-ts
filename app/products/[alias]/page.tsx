b6ac44f4128c5bbe5e945dc583ed434575ccbf70
Zhila <ministrov2018@gmail.com>
2025-01-10 13:29:38 +0300

    fix - Try to find out why error accur when npm run build do

--- app/products/[alias]/page.tsx
+++ app/products/[alias]/page.tsx
@@ -1,24 +1,22 @@
 import { Metadata } from 'next';
-// import { PageProps } from '@/.next/types/app/page';
-import { notFound } from 'next/navigation';
-import { getPage } from '@/api/page';
-import { getMenu } from '@/api/menu';
+// import { notFound } from 'next/navigation';
+// import { getPage } from '@/api/page';
+// import { getMenu } from '@/api/menu';
 
 export const metadata: Metadata = {
     title: 'Страница'
 }
 
-export const generateStaticParams = async () => {
-    const menu = await getMenu(0);
-    return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })));
-}
+// export const generateStaticParams = async () => {
+//     const menu = await getMenu(0);
+//     return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })));
+// }
 
 export default async function PageProduct({ params }: { params: { alias: string } }) {
-    // const { alias } = params;
-    const page = await getPage(params.alias);
-    if (!page) {
-        notFound();
-    }
+    // const page = await getPage(params.alias);
+    // if (!page) {
+    //     notFound();
+    // }
     return (
         <div>{`Страница с alias ${params.alias}`}</div>
     )
