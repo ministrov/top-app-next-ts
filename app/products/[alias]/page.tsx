740f6bc406db3c034aa3fe54205705fc08987317
Zhila <ministrov2018@gmail.com>
2025-01-10 14:02:06 +0300

    fix - Keep on fixing on page products alias error

--- app/products/[alias]/page.tsx
+++ app/products/[alias]/page.tsx
@@ -1,18 +1,18 @@
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 // import { notFound } from 'next/navigation';
 // import { getPage } from '@/api/page';
 // import { getMenu } from '@/api/menu';
 
-export const metadata: Metadata = {
-    title: 'Страница'
-}
+// export const metadata: Metadata = {
+//     title: 'Страница'
+// }
 
 // export const generateStaticParams = async () => {
 //     const menu = await getMenu(0);
 //     return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })));
 // }
 
-export default async function PageProduct({ params }: { params: { alias: string } }) {
+export default async function PageProducts({ params }: { params: { alias: string } }) {
     // const page = await getPage(params.alias);
     // if (!page) {
     //     notFound();
