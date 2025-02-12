f8e17a9e142893dec247116674d413c288e13699
Zhila <ministrov2018@gmail.com>
2025-02-12 19:05:14 +0300

    fix - Fixing an issue with a types of incorrect params typing

--- app/products/[alias]/page.tsx
+++ app/products/[alias]/page.tsx
@@ -1,19 +1,36 @@
-// import { Metadata } from 'next';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { getPage } from '@/api/page';
 // import { getMenu } from '@/api/menu';
 
-// export const metadata: Metadata = {
-//     title: 'Страница'
-// }
+type PageData = {
+    id?: number,
+    alias: string,
+    title: string
+}
+
+type PageProps = {
+    params: {
+        alias: string
+    }
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+    const page = await getPage(params.alias);
+
+    return {
+        title: page?.title || 'Page Not Found',
+        description: page?.metaDescription || ''
+    }
+}
 
 // export const generateStaticParams = async () => {
 //     const menu = await getMenu(0);
 //     return menu.flatMap(item => item.pages.map(page => ({ alias: page.alias })));
 // }
 
-export default async function PageProducts({ params }: { params: { alias: string } }) {
-    const page = await getPage(params.alias);
+export default async function PageProducts({ params }: PageProps) {
+    const page: PageData | null = await getPage(params.alias);
     console.log(page);
     if (!page) {
         notFound();
