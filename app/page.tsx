132a3db04bad4f348012c1137f6011501343ee17
Zhila <ministrov2018@gmail.com>
2025-02-12 16:42:36 +0300

    feature - Add some code to practise

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { Icon } from './assets/Icon';
 import { Button } from './components/Button/Button';
 
 export default async function Home() {
+  console.log('I am home');
+  const a: string = 'I am at home';
+
+  console.log(`text: ${a}`);
   return (
     <div>
       {'Home Page'}
