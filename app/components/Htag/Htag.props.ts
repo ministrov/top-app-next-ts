d592b13f9898a87e677fd071ab4cf577ccdafbca
Zhila <ministrov2018@gmail.com>
2025-02-12 12:31:28 +0300

    fix - Improve a type annotation for Htag component

--- app/components/Htag/Htag.props.ts
+++ app/components/Htag/Htag.props.ts
@@ -1,6 +1,6 @@
-import { ReactNode } from 'react';
+import { ReactNode, DetailedHTMLProps, HTMLElementType, HTMLAttributes } from 'react';
 
-export interface HtagProps {
+export interface HtagProps extends DetailedHTMLProps<HTMLAttributes<HTMLElementType>, HTMLElementType> {
     tag: 'h1' | 'h2' | 'h3';
     children: ReactNode; 
 }
