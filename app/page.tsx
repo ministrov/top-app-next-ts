93c15f327bc3b7223d0508202477565e7d219e05
Zhila <ministrov2018@gmail.com>
2025-02-13 17:24:04 +0300

    fix - Keep on operating on creating multylevel menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-// import { getMenu } from '@/api/menu';
 import { Icon } from './assets/Icon';
 import { Button } from './components/Button/Button';
 
 export default async function Home() {
-  // throw new Error('dfsdfsdfs');
-  // const menu = await getMenu(0);
   return (
     <div>
       {'Home Page'}
@@ -15,8 +12,6 @@ export default async function Home() {
 
       <Button arrow='down' appearence={'primary'}>{'fdfdfdf'}</Button>
       <Button arrow='right' appearence={'ghost'}>{'fdfdfdf'}</Button>
-
-      {/* {menu.length} */}
     </div>
   );
 }
