01627d16719a9cd51532c914d6b3c063f4357893
Zhila <ministrov2018@gmail.com>
2025-02-25 15:31:10 +0300

    feature - Create card component

---
+++ app/components/Card/Card.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+// import cn from 'classnames';
+import styles from './Button.module.css';
+
+export const Card = () => {
+    return (
+        <div className={styles.Card}>
+
+        </div>
+    )
+}
