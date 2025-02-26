2dfb1924cb582938658983a95d42c0da8a36da6d
Zhila <ministrov2018@gmail.com>
2025-02-26 16:54:19 +0300

    feature - Keep on developing sorting component

--- app/components/Sorting/Sorting.tsx
+++ app/components/Sorting/Sorting.tsx
@@ -1,4 +1,6 @@
-import { SortingProps } from './Sorting.props';
+'use client';
+
+import { SortEnum, SortingProps } from './Sorting.props';
 import { Icon } from '@/app/assets/Icon';
 import cn from 'classnames';
 import styles from './Sorting.module.css';
@@ -9,8 +11,23 @@ export const Sorting = ({ sort, setSort, className, ...props }: SortingProps) =>
     }
     return (
         <div className={cn(styles.sorting, className)} {...props}>
-            <span className={styles.iconBox}>{<Icon.SortingIcon />}</span>
-            Sorting
+            <span
+                onClick={() => setSort(SortEnum.Rating)}
+                className={styles.iconBox}
+            >
+                <span className={styles.iconWrap}>
+                    <Icon.SortingIcon />
+                </span> По рейтингу
+            </span>
+
+            <span
+                onClick={() => setSort(SortEnum.Price)}
+                className={styles.iconBox}
+            >
+                <span className={styles.iconWrap}>
+                    <Icon.SortingIcon />
+                </span> По цене
+            </span>
         </div>
     )
 }
