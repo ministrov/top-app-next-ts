151ecf349a6078b10f4728ae9ef2bfb3f05ed692
Zhila <ministrov2018@gmail.com>
2025-03-03 12:34:46 +0300

    feature - Start developing search component

---
+++ app/components/Searching/Searching.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { SearchingProps } from './Searching.props';
+// import cn from 'classnames';
+import styles from './Searching.module.css';
+
+const Searching = () => {
+    return (
+        <div className={styles.search}>Search</div>
+    )
+}
+
+export default Searching;
