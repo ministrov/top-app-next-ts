62dc5ba2a3ea30b223d667d087fa44cd5e7df431
Zhila <ministrov2018@gmail.com>
2025-03-07 13:36:28 +0300

    fix - Checking types

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,6 @@
 import { Icon } from './assets/Icon';
 import { Button } from './components/Button/Button';
 
-type Item = {
-  id: string;
-};
-
-type Post = Item & {
-  title: string;
-  description: string;
-}
-
-type MegaPost = Post & {
-  viewsCount: number;
-}
-
-const myItem: Item | MegaPost = { id: '31337nb' };
-const myViewsCount: MegaPost = { viewsCount: 6, id: 'sfsdfsd', title: 'sfsdf', description: 'sfsdfsd' };
-
-function printId<T extends Item>(post: T): void {
-  console.log(post.id);
-}
-
-printId(myItem);
-printId(myViewsCount);
-
 export default async function Home() {
   return (
     <div>
@@ -35,6 +12,8 @@ export default async function Home() {
 
       <Button arrow='down' appearence={'primary'}>{'fdfdfdf'}</Button>
       <Button arrow='right' appearence={'ghost'}>{'fdfdfdf'}</Button>
+
+      <button>Button</button>
 
     </div>
   );
