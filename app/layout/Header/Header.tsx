047d37b7c493dadd17f68ce8652bce0ac6164aba
Zhila <ministrov2018@gmail.com>
2025-03-14 10:30:57 +0300

    feature - Keep on operatin on creating header mobile menu

--- app/layout/Header/Header.tsx
+++ app/layout/Header/Header.tsx
@@ -1,11 +1,18 @@
 import { HeaderProps } from './Header.props';
+import { Icon } from '@/app/assets/Icon';
 import cn from 'classnames';
 import styles from './Header.module.css';
+import { ButtonIcon } from '@/app/components/ButtonIcon/ButtonIcon';
 
 export const Header = ({ className, ...props }: HeaderProps) => {
     return (
         <header className={cn(className, styles.header)} {...props}>
+            <Icon.LogoIcon className={styles.logo} />
 
+            <ButtonIcon
+                appearence='white'
+                icon='MenuIcon'
+            />
         </header>
     )
 }
