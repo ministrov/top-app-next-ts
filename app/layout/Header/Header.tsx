398f798355b163e1af58b48f06527d6056027830
Zhila <ministrov2018@gmail.com>
2025-03-14 11:41:37 +0300

    feature - End up creating and styling mobile menu

--- app/layout/Header/Header.tsx
+++ app/layout/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { ButtonIcon } from '@/app/components/ButtonIcon/ButtonIcon';
 import { Sidebar } from '../Sidebar/Sidebar';
@@ -9,19 +10,42 @@ import cn from 'classnames';
 import styles from './Header.module.css';
 
 export const Header = ({ className, ...props }: HeaderProps) => {
+    const [isOpened, setIsOpened] = useState<boolean>(false);
+
+    const variants = {
+        opened: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                stiffness: 20
+            }
+        },
+        closed: {
+            opacity: 0,
+            x: '100%',
+        }
+    };
     return (
         <header className={cn(className, styles.header)} {...props}>
             <Icon.LogoIcon className={styles.logo} />
 
             <ButtonIcon
+                onClick={() => setIsOpened(true)}
                 appearence='white'
                 icon='MenuIcon'
             />
 
-            <motion.div className={styles.mobileMenu}>
+            <motion.div
+                initial={'closed'}
+                variants={variants}
+                animate={isOpened ? 'opened' : 'closed'}
+                className={styles.mobileMenu}
+            >
                 <Sidebar className={styles.sidebar} />
 
                 <ButtonIcon
+                    onClick={() => setIsOpened(false)}
+                    className={styles.menuClose}
                     appearence='white'
                     icon='MenuClose'
                 />
