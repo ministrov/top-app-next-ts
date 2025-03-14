1c61d4fb45e4f543c4fe7ae4a2a6418df6612466
Zhila <ministrov2018@gmail.com>
2025-03-14 17:24:32 +0300

    feature - Add animation for button component and use hook useMotionValue to check the scale value

--- app/components/Button/Button.props.ts
+++ app/components/Button/Button.props.ts
@@ -1,6 +1,7 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from 'react';
 
-export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+export interface ButtonProps extends 
+    Omit<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> , 'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onDrag' | 'ref' >{
     appearence: 'primary' | 'ghost';
     children: ReactNode;
     arrow?: 'right' | 'down' | 'none';
